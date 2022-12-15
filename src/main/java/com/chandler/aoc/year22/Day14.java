9f873742a7f0b027a03dae2349dde3c7de809322
Chandler Klein <kleinc16@gmail.com>
2022-12-15 00:38:26 -0600

    Day 14 cringe

--- src/main/java/com/chandler/aoc/year22/Day14.java
+++ src/main/java/com/chandler/aoc/year22/Day14.java
@@ -13,16 +13,48 @@ public class Day14 extends Day {
         new Day14().printParts();
     }
 
+    private static int maxRow = -1;
+
     @Override
     protected Object part1() {
         String[] lines = dayString().split("\r\n");
-
         char[][] grid = getGrid();
         parseAndDraw(lines, grid);
+        return dropSand(grid, 0);
+    }
 
-        printGrid(grid);
+    private int dropSand(char[][] grid, int numSandPieces) {
+        int[] sandPosition = new int[]{0, 500};
+        int numMoves = 0;
 
-        return null;
+        while (sandCanFall(sandPosition, grid)) {
+            if (numMoves > 500) {
+                return numSandPieces;
+            }
+            if (isSpaceOpen(grid[sandPosition[0] + 1][sandPosition[1]])) {
+                sandPosition[0]++;
+            } else if (isSpaceOpen(grid[sandPosition[0] + 1][sandPosition[1] - 1])) {
+                sandPosition[0]++;
+                sandPosition[1]--;
+            } else {
+                sandPosition[0]++;
+                sandPosition[1]++;
+            }
+            numMoves++;
+        }
+        grid[sandPosition[0]][sandPosition[1]] = 'o';
+        numSandPieces++;
+        return dropSand(grid, numSandPieces);
+    }
+
+    private boolean sandCanFall(int[] sandPosition, char[][] grid) {
+        int row = sandPosition[0];
+        int col = sandPosition[1];
+        return isSpaceOpen(grid[row + 1][col - 1]) || isSpaceOpen(grid[row + 1][col]) || isSpaceOpen(grid[row + 1][col + 1]);
+    }
+
+    private boolean isSpaceOpen(char space) {
+        return space != '#' && space != 'o';
     }
 
     private void parseAndDraw(String[] lines, char[][] grid) {
@@ -34,37 +66,30 @@ public class Day14 extends Day {
 
     private void drawRockPaths(List<RockPath> rockPaths, char[][] grid) {
         for (int i = 0; i < rockPaths.size() - 1; i++) {
-            int fromX = rockPaths.get(i).x();
-            int fromY = rockPaths.get(i).y();
+            int fromRow = rockPaths.get(i).row();
+            int fromCol = rockPaths.get(i).col();
 
-            int toX = rockPaths.get(i + 1).x();
-            int toY = rockPaths.get(i + 1).y();
+            int toRow = rockPaths.get(i + 1).row();
+            int toCol = rockPaths.get(i + 1).col();
 
-            while (fromX != toX || fromY != toY) {
-                grid[fromX][fromY] = '#';
-                if (fromX < toX) {
-                    fromX++;
-                } else if (fromY < toY) {
-                    fromY++;
-                } else if (fromX > toX) {
-                    fromX--;
+            while (fromRow != toRow || fromCol != toCol) {
+                grid[fromRow][fromCol] = '#';
+                if (fromRow < toRow) {
+                    fromRow++;
+                } else if (fromCol < toCol) {
+                    fromCol++;
+                } else if (fromRow > toRow) {
+                    fromRow--;
                 } else {
-                    fromY--;
+                    fromCol--;
                 }
             }
-            grid[fromX][fromY] = '#';
-        }
-    }
-
-    private static void printGrid(char[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            char[] chars = grid[i];
-            System.out.printf("%-3d %s%n", i, Arrays.toString(chars));
+            grid[fromRow][fromCol] = '#';
         }
     }
 
     private char[][] getGrid() {
-        char[][] grid = new char[500][600];
+        char[][] grid = new char[1_000][1_100];
         Arrays.stream(grid).forEach(chars -> Arrays.fill(chars, '.'));
         grid[0][500] = '+';
         return grid;
@@ -79,19 +104,55 @@ public class Day14 extends Day {
         }
     }
 
-    private record RockPath(int x, int y) {
+    private record RockPath(int row, int col) {
         private static RockPath createRockPath(int distanceRight, int distanceDown) {
+            maxRow = Math.max(maxRow, distanceDown);
             return new RockPath(distanceDown, distanceRight);
         }
 
         @Override
         public String toString() {
-            return "(%d,%d)".formatted(x, y);
+            return "(%d,%d)".formatted(row, col);
         }
     }
 
     @Override
     protected Object part2() {
-        return null;
+        String[] lines = dayString().split("\r\n");
+        char[][] grid = getGrid();
+        parseAndDraw(lines, grid);
+        drawFloor(grid, maxRow + 2);
+        return dropSandPart2(grid);
+    }
+
+    private void drawFloor(char[][] grid, int rowNum) {
+        for (int i = 0; i < grid[0].length; i++) {
+            grid[rowNum][i] = '#';
+        }
+    }
+
+    private int dropSandPart2(char[][] grid) {
+        int numSandPieces = 0;
+        int[] sandPosition = new int[]{0, 500};
+
+        while (true) {
+            while (sandCanFall(sandPosition, grid)) {
+                if (isSpaceOpen(grid[sandPosition[0] + 1][sandPosition[1]])) {
+                    sandPosition[0]++;
+                } else if (isSpaceOpen(grid[sandPosition[0] + 1][sandPosition[1] - 1])) {
+                    sandPosition[0]++;
+                    sandPosition[1]--;
+                } else {
+                    sandPosition[0]++;
+                    sandPosition[1]++;
+                }
+            }
+            grid[sandPosition[0]][sandPosition[1]] = 'o';
+            numSandPieces++;
+            if (sandPosition[0] == 0 && sandPosition[1] == 500) {
+                return numSandPieces;
+            }
+            sandPosition = new int[]{0, 500};
+        }
     }
 }
