3f498e89a9311eaec95fc35d6951f4b965e4eb0d
Chandler Klein <kleinc16@gmail.com>
2022-12-19 21:56:09 -0600

    Add Day 4

---
+++ src/main/java/com/chandler/aoc/year20/Day04.java
@@ -0,0 +1,33 @@
+package com.chandler.aoc.year20;
+
+import com.chandler.aoc.common.Day;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public class Day04 extends Day {
+
+    public static void main(String[] args) {
+        new Day04().printParts();
+    }
+
+    private static final Set<String> requiredFields = Set.of("byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:");
+
+    @Override
+    protected Object part1() {
+        return getNumInvalidPassports();
+    }
+
+    private long getNumInvalidPassports() {
+        String[] passports = dayString().split("\r\n\r\n");
+
+        return passports.length - Arrays.stream(passports)
+                                        .filter(passport -> requiredFields.stream().anyMatch(field -> !passport.contains(field)))
+                                        .count();
+    }
+
+    @Override
+    protected Object part2() {
+        return null;
+    }
+}
