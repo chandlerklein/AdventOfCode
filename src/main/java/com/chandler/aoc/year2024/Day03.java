e052e1ac8d9415a0082b0e170dcc4b0568f86984
Chandler Klein <kleinc16@gmail.com>
2024-12-03 01:03:35 -0600

    Day03 pattern matching

--- src/main/java/com/chandler/aoc/year2024/Day03.java
+++ src/main/java/com/chandler/aoc/year2024/Day03.java
@@ -3,9 +3,9 @@ package com.chandler.aoc.year2024;
 import com.chandler.aoc.common.Day;
 
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.regex.Pattern;
 
 import static java.lang.Long.parseLong;
+import static java.util.regex.Pattern.compile;
 
 public class Day03 extends Day {
 
@@ -14,30 +14,27 @@ public class Day03 extends Day {
     }
 
     @Override protected Object part1() {
-        return Pattern.compile("mul\\((\\d+),(\\d+)\\)").matcher(string())
-                      .results()
-                      .map(result -> parseLong(result.group(1)) * parseLong(result.group(2)))
-                      .mapToLong(Long::longValue)
-                      .sum();
+        return compile("mul\\((\\d+),(\\d+)\\)")
+            .matcher(string()).results()
+            .map(result -> parseLong(result.group(1)) * parseLong(result.group(2)))
+            .mapToLong(Long::longValue)
+            .sum();
     }
 
     @Override protected Object part2() {
         var enabled = new AtomicBoolean(true);
-        return Pattern.compile("mul\\((\\d+),(\\d+)\\)|do(n't)?\\(\\)").matcher(string())
-                      .results()
-                      .<Long>mapMulti((result, mapper) -> {
-                          switch (result.group(0)) {
-                              case "do()" -> enabled.set(true);
-                              case "don't()" -> enabled.set(false);
-                              default -> {
-                                  if (enabled.get()) {
-                                      mapper.accept(parseLong(result.group(1)) * parseLong(result.group(2)));
-                                  }
-                              }
-                          }
-                      })
-                      .mapToLong(Long::longValue)
-                      .sum();
+        return compile("mul\\((\\d+),(\\d+)\\)|do(n't)?\\(\\)")
+            .matcher(string()).results()
+            .<Long>mapMulti((it, mapper) -> {
+                switch (it.group(0)) {
+                    case "do()" -> enabled.set(true);
+                    case "don't()" -> enabled.set(false);
+                    case String _ when enabled.get() -> mapper.accept(parseLong(it.group(1)) * parseLong(it.group(2)));
+                    default -> { /* ignore */ }
+                }
+            })
+            .mapToLong(Long::longValue)
+            .sum();
     }
 
 }
