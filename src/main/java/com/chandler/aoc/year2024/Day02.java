fca8d265116ca9132a3920e03470c76859bc7b5f
Chandler Klein <kleinc16@gmail.com>
2024-12-02 22:22:31 -0600

    update part2

--- src/main/java/com/chandler/aoc/year2024/Day02.java
+++ src/main/java/com/chandler/aoc/year2024/Day02.java
@@ -1,10 +1,15 @@
 package com.chandler.aoc.year2024;
 
 import com.chandler.aoc.common.Day;
+import com.google.common.collect.Range;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
-import static java.lang.Math.abs;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Gatherers.windowSliding;
 
 public class Day02 extends Day {
 
@@ -13,26 +18,32 @@ public class Day02 extends Day {
     }
 
     @Override protected Object part1() {
-        return stream().map(line -> Arrays.stream(line.split("\\s")).mapToInt(Integer::parseInt).toArray())
+        return stream().map(line -> Arrays.stream(line.split("\\s")).mapToInt(Integer::parseInt).boxed().toList())
                        .filter(this::isSafePart1)
                        .count();
     }
 
     @Override protected Object part2() {
-        return stream().map(line -> Arrays.stream(line.split("\\s")).mapToInt(Integer::parseInt).toArray())
+        return stream().map(line -> Arrays.stream(line.split("\\s")).mapToInt(Integer::parseInt).boxed().toList())
+                       .filter(this::isSafePart2)
                        .count();
     }
 
-    private boolean isSafePart1(int[] line) {
-        int firstDiff = line[1] - line[0];
-        if (firstDiff == 0 || abs(firstDiff) > 3) return false;
-        boolean isIncreasing = firstDiff > 0;
-        for (int i = 2; i < line.length; i++) {
-            int diff = line[i] - line[i - 1];
-            if (isIncreasing && (diff > 3 || diff < 1)) return false;
-            if (!isIncreasing && (diff < -3 || diff > -1)) return false;
-        }
-        return true;
+    private boolean isSafePart1(List<Integer> line) {
+        var diffs = line.stream().gather(windowSliding(2))
+                        .map(window -> window.getLast() - window.getFirst())
+                        .collect(toSet());
+        return Range.closed(1, 3).containsAll(diffs) || Range.closed(-3, -1).containsAll(diffs);
+    }
+
+    // I give up :^)
+    private boolean isSafePart2(List<Integer> line) {
+        if (isSafePart1(line)) return true;
+        return IntStream.range(0, line.size()).anyMatch(i -> {
+            List<Integer> sublist = new ArrayList<>(line);
+            sublist.remove(i);
+            return isSafePart1(sublist);
+        });
     }
 
 }
